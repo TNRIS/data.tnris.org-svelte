1c1624dba2e35d91166fcc308e76f119aa3a49b9
ctrepka <chris.repka@twdb.texas.gov>
2023-01-24 13:10:31 -0600

    Create switch component specifically designed for maplayers

---
+++ src/lib/Components/Map/LayerSwitch.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Switch from "../General/Switch.svelte";
+
+  export let checked: boolean = false;
+</script>
+
+<div class="layer-switch">
+  <span class="switch-label"><slot /> </span>
+  <Switch bind:checked />
+</div>
+
+<style lang="scss">
+  .layer-switch {
+    display: grid;
+    gap: 0.5rem;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    justify-content: center;
+    height: fit-content;
+  }
+</style>
