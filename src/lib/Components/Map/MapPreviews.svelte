284fae1b587e161f70ee193e876792a7132c6121
ctrepka <chris.repka@twdb.texas.gov>
2023-01-24 13:10:55 -0600

    revise styling of layerswitch container for map previews

--- src/lib/Components/Map/MapPreviews.svelte
+++ src/lib/Components/Map/MapPreviews.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-
+  import LayerSwitch from "./LayerSwitch.svelte";
 </script>
 
 <div id="map-previews">
-    
+  <LayerSwitch>Imagery Layer</LayerSwitch>
 </div>
 
 <style lang="scss">
-    #map-previews {
-        position: absolute;
-        top: .5rem;
-        left: .5rem;
-        display: grid;
-        padding: .25rem;
-        border: 1px solid $borderColor;
-        z-index: 999999;
-        background: white;
-        width: 4rem;
-        height: 4rem;
-        border-radius: .25rem;
-        box-shadow: $boxShadow-xs;
-    }
+  #map-previews {
+    position: absolute;
+    top: 0.5rem;
+    left: 0.5rem;
+    display: grid;
+    padding: 0.5rem;
+    border: 1px solid $borderColor;
+    z-index: 999999;
+    background: white;
+    width: fit-content;
+    min-width: 40px;
+    height: fit-content;
+    border-radius: 0.25rem;
+    box-shadow: $boxShadow-xs;
+  }
 </style>
