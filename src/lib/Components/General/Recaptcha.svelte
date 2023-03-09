1d09398941b43d635ebe5c5baf70316783e2bf8e
lh <111371248+L-Har@users.noreply.github.com>
2023-03-09 08:22:44 -0600

    status integration

--- src/lib/Components/General/Recaptcha.svelte
+++ src/lib/Components/General/Recaptcha.svelte
@@ -1,21 +1,26 @@
 <script>
   import { RECAPTCHA_SITE_KEY } from "../../constants";
   import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
 
+  const dispatcher = createEventDispatcher();
   export let recaptchaId = "grecaptcha-default-id"
   let recaptchaContainer = null;
 
   export let handleCaptchaCallback = async (v) => {
+    dispatcher("success", { msg: "okay", token: v });
     //console.log("DEFAULT HANDLE CAPTCHA CALLBACK", v);
     return;
   };
 
   export let handleCaptchaError = async (v) => {
+    dispatcher("error", { msg: "error", token: v });
     //console.log("DEFAULT HANDLE CAPTCHA ERROR", v);
     return;
   };
 
   export let resetCaptcha = async (v) => {
+    dispatcher("reset", { msg: "reset", token: v });
     //console.log("DEFAULT RESET CAPTCHA", v);
     // @ts-ignore
     window.grecaptcha.reset(recaptchaContainer);
@@ -30,10 +35,9 @@
         sitekey: RECAPTCHA_SITE_KEY,
         callback: handleCaptchaCallback,
         "expired-callback": resetCaptcha,
-        "error-ballback": handleCaptchaError,
+        "error-ballback": handleCaptchaError
       });
     });
-    // @ts-ignore
   });
 </script>
 
--- src/lib/Components/Order/OrderAuth.svelte
+++ src/lib/Components/Order/OrderAuth.svelte
@@ -2,14 +2,61 @@
   import { query } from "svelte-pathfinder";
   import InfoBox from "../General/InfoBox.svelte";
   import Recaptcha from "../General/Recaptcha.svelte";
-
-  const order_id = $query.params["order_id"];
+  import { BASE_URL } from "../../constants.js"
+  const order_id = $query.params["uuid"];
 
   let authenticated = false;
   let statusData = null;
+  let otp_gened = false;
+  let captcha_token = "";
+  let toggle  = true;
+  let show_otp_msg = true;
+  
+  function storeCaptchaToken(resp) {
+    if(resp && resp.detail && resp.detail.token) {
+      captcha_token = resp.detail.token
+    } else {
+      captcha_token = ""
+    }
+  }
+
+  function onCaptchaSuccess(resp) {
+    toggle = false;
+    storeCaptchaToken(resp)
+    
+    if(captcha_token && captcha_token.length) {
+      genotp(captcha_token);
+    }
+  }
+
+  function onCaptchaTimeout() {
+    toggle = true;
+  }
+
+  function genotp(resp) {
+    if(!otp_gened) {
+      otp_gened = true;
+      const payload = {
+        recaptcha: resp,
+      };
+
+      fetch(
+        `${BASE_URL}/api/v1/contact/order/otp?uuid=${order_id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify(payload),
+        }
+      );
+    }
+  }
 
   export let onSubmit = async (e) => {
     e.preventDefault();
+
+    show_otp_msg = false;
     const fd = new FormData(e.target);
 
     const d = Object.fromEntries(fd);
@@ -17,11 +64,11 @@
     const payload = {
       accessCode: d["email"],
       passCode: d["otp"],
-      recaptcha: d["g-recaptcha-response"],
+      recaptcha: captcha_token,
     };
 
     const resp = await fetch(
-      `https://stagingapi.tnris.org/api/v1/contact/order/status?uuid=${order_id}`,
+      `${BASE_URL}/api/v1/contact/order/status?uuid=${order_id}`,
       {
         headers: {
           "Content-Type": "application/json",
@@ -41,7 +88,16 @@
     e.target.reset();
   };
 </script>
+  {#if toggle}
+    <div id="recaptcha">
+      <Recaptcha 
+        on:success={onCaptchaSuccess}
+        on:reset={onCaptchaTimeout}
+      />
+    </div>
+  {/if}
 
+{#if !toggle}
 <div id="order-auth">
   {#if statusData}
     <InfoBox infoClass={statusData.status == "failure" ? "caution" : "info"}>
@@ -53,41 +109,47 @@
   {/if}
   {#if !authenticated}
     <h1>Order Status Login</h1>
-    <InfoBox infoClass="info">
-      You must use a one time password and your email address to access your
-      order status. You can use the form to the right to get a new one time
-      password sent to your email address.
-    </InfoBox>
+    {#if show_otp_msg}
+      <InfoBox infoClass="info">
+        A One time Passcode has been sent to the email this order is registered under. Please enter it below.
+      </InfoBox>
+    {/if}
     <form
       id="order-auth-form"
       on:submit|preventDefault|stopPropagation={onSubmit}
     >
-      <label class="required">
-        Email Address
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="email address"
-          required
-        />
-      </label>
-      <label class="required"
-        >One Time Password
-        <input
-          type="password"
-          name="otp"
-          id="otp"
-          placeholder="one time password"
-          required
-        />
-      </label>
-      <Recaptcha />
+    <label class="required"
+    >One Time Access Code
+    <input
+      type="password"
+      name="otp"
+      id="otp"
+      placeholder="one time access code"
+      required
+    />
+  </label>
+
+  <label class="required">
+    Email Address
+    <input
+      type="email"
+      name="email"
+      id="email"
+      placeholder="email address"
+      required
+    />
+  </label>
+  <Recaptcha 
+    on:success={onCaptchaSuccess}
+    on:reset={onCaptchaTimeout}
+  />
       <hr />
-      <button type="submit">Check Order Status</button>
+      <button id="order_button" type="submit">Check Order Status</button>
     </form>
   {/if}
 </div>
+{/if}
+
 
 <style lang="scss">
   #order-auth {
@@ -101,5 +163,9 @@
       display: block;
       margin-bottom: 0.5rem;
     }
+  }
+  #order_button:hover {
+    font-weight: 100; /* Overwrite bold to none and simulate instead since it causes button shifting. */
+    text-shadow: 0 0 .65px #333, 0 0 .65px #333; /* Simulate bold without the shift. */
   }
 </style>
