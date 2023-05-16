1cfaf249fefee6eb9475c6d16711ef466ca9cc83
ctrepka <chris.repka@twdb.texas.gov>
2023-05-16 15:33:06 -0500

    chng buildDir assetDir for build; gitignore build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-//  ,server:{
-//    port:3000,
-//    strictPort:false,
-//    },
-})
+  plugins: [svelte()],
+  build: {
+    outDir: "build",
+    assetsDir: "static"
+  },
+  //  ,server:{
+  //    port:3000,
+  //    strictPort:false,
+  //    },
+});
