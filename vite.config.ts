051881adf36385873d6ed75ec2445e2b76503952
lh <111371248+L-Har@users.noreply.github.com>
2023-05-17 15:32:30 -0500

    debug rough commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodeLoaderPlugin } from "@vavite/node-loader/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-//  ,server:{
-//    port:3000,
-//    strictPort:false,
-//    },
+  plugins: [svelte(),
+    nodeLoaderPlugin(),
+  ],
+  build: {
+    sourcemap: true,
+  }
+  ,server:{
+    port:3000,
+    strictPort:false,
+    },
 })
