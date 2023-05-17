0d988f0707dae89a08c860cebed7ce9ec5794489
lh <111371248+L-Har@users.noreply.github.com>
2023-05-17 16:02:00 -0500

    Node loader is not needed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { nodeLoaderPlugin } from "@vavite/node-loader/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),
-    nodeLoaderPlugin(),
+  plugins: [svelte()
   ],
   build: {
     sourcemap: true,
