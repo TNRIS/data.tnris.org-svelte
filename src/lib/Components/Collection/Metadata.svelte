bcae28b6ce461e7d07267764cb4c4b994c54e0fb
ctrepka <chris.repka@twdb.texas.gov>
2023-06-06 13:48:40 -0500

    collection metadata view updates

--- src/lib/Components/Collection/Metadata.svelte
+++ src/lib/Components/Collection/Metadata.svelte
@@ -56,15 +56,10 @@
       <section class="metdata-section">
         <h2>About the Historic Imagery Archive</h2>
         <p>
-          The Historical Imagery Archive maintained by TNRIS is one of our most
-          used and important data collections. It is comprised of over 1 million
-          frames of photos covering all parts of Texas from dates as far back as
-          the 1920s.
+          The Historical Imagery Archive maintained by TNRIS is one of our most used and important data collections. It is comprised of over 1 million frames of photos covering all parts of Texas from dates as far back as the 1920s.
         </p>
         <p>
-          The TNRIS Research & Distribution Center (RDC) is charged with
-          preserving this collection, distributing it to the public, and
-          continuing with the large task of digitizing the frames.
+          The TNRIS Research & Distribution Center (RDC) is charged with preserving this collection, distributing it to the public, and continuing with the large task of digitizing the frames.
         </p>
       </section>
       {#each orderedHistoricFields as field}
@@ -83,12 +78,7 @@
         <section class="metadata-section">
           <h2>wms link</h2>
           <p>
-            This dataset is available as an online mapping service. An OGC WMS
-            service and an ArcGIS service are available. To connect to the WMS
-            service in your software, please copy the unique url provided in the
-            box below. To access the TNRIS ArcGIS Server, please use the
-            following url in your ESRI software and select from the list of
-            available services.
+            This dataset is available as an online mapping service. An OGC WMS service and an ArcGIS service are available. To connect to the WMS service in your software, please copy the unique url provided in the box below. To access the TNRIS ArcGIS Server, please use the following url in your ESRI software and select from the list of available services.
           </p>
           <CopyLink
             href={collection.index_service_url}
@@ -128,22 +118,11 @@
         <section class="metadata-section">
           <h2>wms link</h2>
           <p>
-            This dataset is available as an online mapping service. An OGC WMS
-            service and an ArcGIS service are available. To connect to the WMS
-            service in your software, please copy the unique url provided in the
-            box below. To access the TNRIS ArcGIS Server, please use the
-            following url in your ESRI software and select from the list of
-            available services.
+            This dataset is available as an online mapping service. An OGC WMS service and an ArcGIS service are available. To connect to the WMS service in your software, please copy the unique url provided in the box below. To access the TNRIS ArcGIS Server, please use the following url in your ESRI software and select from the list of available services.
           </p>
 
           <p>
-            For ESRI users accessing our RESTful Services hosted on our ArcGIS
-            server (feature.tnris.org or imagery.tnris.org), you may access all
-            WMS services available by downloading our <a
-              href="https://cdn.tnris.org/data/RESTful_Services/TNRIS_RESTful_Services.zip"
-              download>feature and imagery connection files</a
-            > and selecting from the list of availabile services within your ESRI
-            desktop software.
+            For ESRI users accessing our RESTful Services hosted on our ArcGIS server (feature.tnris.org or imagery.tnris.org), you may access all WMS services available by downloading our <a href="https://cdn.tnris.org/data/RESTful_Services/TNRIS_RESTful_Services.zip" download>feature and imagery connection files</a> and selecting from the list of availabile services within your ESRI desktop software.
           </p>
           <CopyLink href={collection.wms_link} title={collection.wms_link} />
           <WmsPreview wms_link={collection.wms_link} />
